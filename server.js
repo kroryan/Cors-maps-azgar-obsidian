15defb7554168110387ef06e7cc4588d26e165f7
kroryan <adrylanube@gmail.com>
2025-05-19 22:23:44 +0200

    local network implementation

--- server.js
+++ server.js
@@ -22,9 +22,10 @@ app.use(cors());
 app.use('/maps', express.static(path.join(__dirname, 'maps')));
 
 // Iniciar el servidor y escuchar en el puerto especificado
-app.listen(PORT, () => {
-  console.log(`Servidor escuchando en el puerto ${PORT}`);
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Servidor escuchando en el puerto ${PORT} en todas las interfaces de red (0.0.0.0)`);
   console.log('Sirviendo archivos desde la carpeta "maps"');
   console.log(`Asegúrate de que la carpeta "maps" exista en el directorio raíz del proyecto y contenga tus archivos .map`);
-  console.log('Para acceder a un mapa, usa una URL como: http://localhost:4040/maps/nombreDelMapa.map');
+  console.log('Para acceder a un mapa desde la misma máquina, usa: http://localhost:4040/maps/nombreDelMapa.map');
+  console.log('Para acceder desde otra máquina en la red, usa: http://[IP-DE-ESTE-SERVIDOR]:4040/maps/nombreDelMapa.map');
 });
